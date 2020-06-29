faf956290eb5113b16443554048904975d762289
ax300 <andre.cavicchiolli@usp.br>
2020-06-29 19:14:12 -0300

    Done

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import json
+import matplotlib.pyplot as plt
+
 
 population = {}
 
@@ -9,6 +11,21 @@ def add_person(v_ori, v_dest): #cria o lista de adjacencia
         population[v_ori].append(v_dest)
     else: #se o no eh novo
         population[v_ori] = [v_dest]
+
+def plot():
+    k_array = []
+    v_array = []
+
+    for k, v in population.items():
+        k_array.append(k)
+        v_array.append(len(v))
+
+    plt.bar(k_array, v_array)
+    plt.xlabel('Pessoa')
+    plt.ylabel('Grau do Nó')
+    plt.xticks(rotation='vertical')
+    plt.show()
+
 
 if __name__ == "__main__":
 
@@ -21,5 +38,4 @@ if __name__ == "__main__":
             v_ori, v_dest = l.replace('\n', '').split(' ')
             add_person(v_ori, v_dest)
 
-    # print(json.dumps(population, indent=4))
-    print(population)
+    plot()
