7dc03124689675df0d3e296f51657a5e0f4da232
ax300 <andre.cavicchiolli@usp.br>
2020-07-12 17:48:13 -0300

    finalizado

--- Main.py
+++ Main.py
@@ -4,25 +4,49 @@ import matplotlib.pyplot as plt
 
 population = {}
 
-def add_person(v_ori, v_dest): #cria o lista de adjacencia 
+def add_person(v_ori, v_dest): # Cria o lista de adjacencia 
     global population
 
-    if v_ori in population: #se o no ja existe
-        population[v_ori].append(v_dest)
-    else: #se o no eh novo
-        population[v_ori] = [v_dest]
+    if v_ori in population: # Se o no ja existe
+        if v_dest not in population[v_ori][1]:
+            population[v_ori][1].append(v_dest)
+    else: # Se o no eh novo
+        population[v_ori] = [False, [v_dest]] # O false indica que o vértice ainda não foi visitado
 
-def plot():
+def plot_individual_degree_nodes():
     k_array = []
     v_array = []
 
     for k, v in population.items():
         k_array.append(k)
-        v_array.append(len(v))
+        v_array.append(len(v[1]))
 
-    plt.bar(k_array, v_array)
+    plt.bar(k_array, v_array) # Mostra somente os 100 primeiros resultados
     plt.xlabel('Pessoa')
     plt.ylabel('Grau do Nó')
+    plt.xticks(rotation='vertical')
+    plt.show()
+
+
+def plot_degree_distribuition():
+    degree_dict = {}
+    x_array = []
+    y_array = []
+
+    # cria array de distribuicao de nos
+    for k, v in population.items(): 
+        if len(v[1]) not in degree_dict:      
+            degree_dict[len(v[1])] = [k]
+        else:
+            degree_dict[len(v[1])].append(k)
+
+    for k, v in degree_dict.items():
+        x_array.append(k)
+        y_array.append(len(v))
+
+    plt.bar(x_array, y_array) # Mostra somente os 100 primeiros resultados
+    plt.xlabel('Grau')
+    plt.ylabel('Nº pessoas')
     plt.xticks(rotation='vertical')
     plt.show()
 
@@ -37,5 +61,6 @@ if __name__ == "__main__":
         for l in f.readlines():
             v_ori, v_dest = l.replace('\n', '').split(' ')
             add_person(v_ori, v_dest)
-
-    plot()
+    # print(population)
+    # plot_individual_degree_nodes()
+    plot_degree_distribuition()
